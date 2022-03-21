941f85580590bc4413bedf03534421c8a29291d0
Igor Rabelo <iarsec02@protonmail.com>
2022-03-21 09:36:06 -0300

    Repetiçao: FOR

--- 6-18032022/calculo_cpf.c
+++ 6-18032022/calculo_cpf.c
@@ -2,7 +2,7 @@
 
 int main(){
     
-    int peso10 = 10, peso11 = 11, total = 0, resto = 0;
+    int peso10 = 10, peso11 = 11, total = 0, resto = 0, confere;
     char cpf[11];
 
     system("clear");
@@ -15,12 +15,48 @@ int main(){
         iCpf[x] = cpf[x] - 48;
     }
 
+    //----------9 digitos-------------------//
+
     for (int i = 0; i <= 8; i++)
     {
-        printf("%d\n", cpf[i]*peso10);
+        total += iCpf[i]*peso10;
         peso10--;
-        
     }
+
+    resto = total%11;
+
+    if (resto < 2){
+        iCpf[9] = 0;
+    }
+    else {
+        iCpf[9] = 11 - resto;
+    }
+
+    total = 0;
+    resto = 0;
+
+    //------------10 digitos-----------------//
+    
+    for (int i = 0; i <= 9; i++)
+    {
+        total += iCpf[i]*peso11;
+        peso11--;
+    }
+
+    resto = total%11;
+
+    if (resto < 2){
+        iCpf[10] = 0;
+    }
+    else {
+        iCpf[10] = 11 - resto;
+    }
+
+    for (int i = 0; i <= 10; i++){
+        printf("%d", iCpf[i]);       
+    }
+
+    printf("\n");
 
     return 0;
 
