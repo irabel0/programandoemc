31a32767c24a64203a30740fa8aa96e811204085
Igor Rabelo <iarsec02@protonmail.com>
2022-03-28 12:05:59 -0300

    Criacao Bibliotec.h

---
+++ 9-28032022/usa_elementos.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "elementos.h"
+
+void main(){
+
+    system("clear");
+
+    separador();
+    pularLinha(1);
+    printf("\t\t\t\t\t Deu certo!");
+    pularLinha(1);
+    separador();
+    pularLinha(2);
+
+}
